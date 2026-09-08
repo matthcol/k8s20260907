1499d528d9fe20754be6ee2adaf1f3a7412f3fc1
matthcol <matthias.colin@gmail.com>
2026-09-08 11:38:58 +0200

    db sts + sql init

--- cinema-application/deploy-stack.ps1
+++ cinema-application/deploy-stack.ps1
@@ -4,5 +4,6 @@ kubectl apply -f api.deployment.yaml
 kubectl apply -f api.service.yaml
 
 # DB
+kubectl create secret generic db-secret --from-env-file db/db.secret
 kubectl create cm db-env --from-env-file db/db.env
 kubectl apply -f db.deployment.yaml
